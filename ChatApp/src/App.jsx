a579f3af9a5de51dfe3a1e193531c2bac5e480ad
chirag142452 <chi.chiragbhardwaj@gmail.com>
2023-10-24 11:16:44 +0530

    added Login Route

--- ChatApp/src/App.jsx
+++ ChatApp/src/App.jsx
@@ -1,46 +1,19 @@
 import { useState ,useEffect} from 'react'
-import io from 'socket.io-client';
-import {nanoid} from "nanoid";
 import './App.css'
- 
+import {Routes,Route} from "react-router-dom" 
+import Home from './components/HomePage/Home'
+
+
+
 function App() {
-  //create a connection with the backend where server is listening
-const socket = io("http://localhost:3000"); 
-
-const [message,setMessage] = useState('');
-
-const [chat,setChat] = useState([]);
-
-useEffect(()=>{
- socket.on('chat',(payload)=>{
-   setChat([...chat,payload]);
- })
-});
-
-//submit handler
-
-const formSubmitHandler = (e)=>{
-  e.preventDefault();
-  socket.emit("chat",{message});
-  setMessage('');
-}
 
 
 
   return (
     <>
-     <h1>Chatty App</h1>
-     <form onSubmit={formSubmitHandler}>
-      <label htmlFor='msg'>Type your message : </label>
-      <input type="text" id = "msg" placeholder='type your chat' value={message} onChange={(e)=>{setMessage(e.target.value)}}/>
-      <button type='submit'>Send</button>
-      
-     </form>
-     {
-        chat.map((data,i)=>{
-         return <p key={i}>{data.message}</p>
-        })
-      }
+     <Routes>
+      <Route path ="/login" Component={Home}/>
+     </Routes>
     </>
   )
 }
